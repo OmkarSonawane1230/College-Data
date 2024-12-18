3c6c483a8fdd34e5dcb04662e1a2859d0b1abb05
Omkar Sonawane <163371960+OmkarSonawane1230@users.noreply.github.com>
2024-12-18 13:53:53 +0530

    Update assign_6.cpp

--- Second_Year/SEM_I/CG/assign_6.cpp
+++ Second_Year/SEM_I/CG/assign_6.cpp
@@ -3,41 +3,41 @@
 using namespace std;
 
 class WalkingMan {
-   int rhx,rhy;
+    int rhx,rhy;
 public:
-		 void draw(int, int);
-		 void draw(int);
+		  void draw(int, int);
+		  void draw(int);
 };
 
 void WalkingMan::draw(int i) {
-   line(20,380,580,380); // platform
+    line(20,380,580,380); // platform
 
-   if(i % 2 == 0){
-       line(25 + i, 380, 35 + i, 340); // left leg
-       line(45 + i, 380, 35 + i, 340); // right leg
-       line(35 + i, 310, 25 + i, 330); // left hand
-       delay(20);
-   }else{
-       line(35 + i, 380, 35 + i, 340);
-       line(35 + i, 310, 40 + i, 330);
-       delay(20);
-   } 
+    if(i % 2 == 0){
+        line(25 + i, 380, 35 + i, 340); // left leg
+        line(45 + i, 380, 35 + i, 340); // right leg
+        line(35 + i, 310, 25 + i, 330); // left hand
+        delay(20);
+    }else{
+        line(35 + i, 380, 35 + i, 340);
+        line(35 + i, 310, 40 + i, 330);
+        delay(20);
+    } 
 
-   line(35 + i, 340, 35 + i, 310); // body
-   circle(35 + i, 300, 10);        // head
-   line(35 + i, 310, 50 + i, 330); // hand
-   line(50 + i, 330, 50 + i, 280); // umbrella stick
-   line(15 + i, 280, 85 + i, 280); // umbrella right
+    line(35 + i, 340, 35 + i, 310); // body
+    circle(35 + i, 300, 10);        // head
+    line(35 + i, 310, 50 + i, 330); // hand
+    line(50 + i, 330, 50 + i, 280); // umbrella stick
+    line(15 + i, 280, 85 + i, 280); // umbrella right
 
-   arc(50 + i, 280, 180, 360, 35); // umbrella body
-   arc(55 + i, 330, 0, 180, 5);    //umbrella handle
+    arc(50 + i, 280, 180, 360, 35); // umbrella body
+    arc(55 + i, 330, 0, 180, 5);    //umbrella handle
 }
 
 void WalkingMan::draw(int x, int y) {
     int j;
-	rhx=x;
-	rhy=y;
-	for(j=0;j<100;j++)
+   	rhx = x;
+	   rhy = y;
+   	for(j = 0; j < 100; j++)
 	{
 	outtextxy(rand()%rhx,rand()%(rhy-50),"|");
 
